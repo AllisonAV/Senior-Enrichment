e3ccb84ab4d67d2b4d4de3dbf97e3d904e9e052f
Allison Alexander <aveleber@hotmailcom>
2017-04-11 13:29:15 -0400

    updated Main.jsx with React-Redux

--- app/main.jsx
+++ app/main.jsx
@@ -1,32 +1,21 @@
 'use strict'
 
 import React from 'react'
-//import {render} from 'react-dom'
-//import { Provider } from 'react-redux'
+import {render} from 'react-dom'
+import { Provider } from 'react-redux'
 
-//import store from './store'
-// import Root from './components/Root'
-// import Index from './components/Index'
-
-
-// this is the original code
-// render (
-//   <Provider store={store}>
-//   	<Index />
-//   </Provider>,
-//   document.getElementById('main')
-// )
-
-// source had <Root/>.  I replaced it with <index>
-//    <Root/>
-
-// I replaced the above with the below
+//my additions
 import ReactDOM from 'react-dom';
 import {  Router
 		, Route
 		, hashHistory
-		, render
+	//	, render
 		, IndexRedirect } from 'react-router';
+// end additions
+
+import store from './store'
+import Root from './components/Root'
+import Index from './components/Index'
 
 import AppContainer from './components/AppContainer';
 import Student    from './components/Student';
@@ -38,9 +27,10 @@ import StudentsContainer    from './components/StudentsContainer';
 import CampusesContainer from './components/CampusesContainer';
 import NewStudentContainer from './components/NewStudentContainer';
 
-
+//this is the original code with my additions - going to try react-redux
 ReactDOM.render (
-	<Router history={hashHistory} >
+  <Provider store={store}>
+  	<Router history={hashHistory} >
 		<Route path='/'  component={AppContainer} foo={'foo'}>
 			<Route path='/campus' component={CampusesContainer} />
 			<Route path='/campus/:campusId' component={Campus} />
@@ -50,10 +40,49 @@ ReactDOM.render (
 			<Route path='/student/new' component={NewStudentContainer} />
 			<IndexRedirect to='/campus' />
 		</Route>
-	</Router>,
-	document.getElementById('main')
-	);
+	</Router>
+  </Provider>,
+  document.getElementById('main')
+)
 
+// source had <Root/>.  I replaced it with <index>
+//    <Root/>
+
+// I replaced the above with the below to use react without redux
+// import ReactDOM from 'react-dom';
+// import {  Router
+// 		, Route
+// 		, hashHistory
+// 		, render
+// 		, IndexRedirect } from 'react-router';
+
+// import AppContainer from './components/AppContainer';
+// import Student    from './components/Student';
+// import Students   from './components/Students'
+// import Campus   from './components/Campus';
+// import Campuses from './components/Campuses';
+
+// import StudentsContainer    from './components/StudentsContainer';
+// import CampusesContainer from './components/CampusesContainer';
+// import NewStudentContainer from './components/NewStudentContainer';
+
+
+// ReactDOM.render (
+// 	<Router history={hashHistory} >
+// 		<Route path='/'  component={AppContainer} foo={'foo'}>
+// 			<Route path='/campus' component={CampusesContainer} />
+// 			<Route path='/campus/:campusId' component={Campus} />
+// 			<Route path='/user'   component={StudentsContainer} />
+// 			<Route path='/user/:userId'  component={Student} />
+// 			<Route path='/campus/user/:campusId'  component={Students} />
+// 			<Route path='/student/new' component={NewStudentContainer} />
+// 			<IndexRedirect to='/campus' />
+// 		</Route>
+// 	</Router>,
+// 	document.getElementById('main')
+// 	);
+
+// end of code that worked without redux
 
 // <Route path="/user"   component={UsersContainer} />
 
